a8466e8bc41adc13abee8a399df31fb0f160e468
ibro45 <ibrahimhadzic45@gmail.com>
2020-03-29 23:39:05 +0200

    train val datasets

--- cough-detector/utils/datasets.py
+++ cough-detector/utils/datasets.py
@@ -4,11 +4,14 @@ from data.melspectrogram_dataset import MelSpectrogramDataset
 def load_dataset(args):
     print('==> Preparing data..')
     
-    train_dataset = MelSpectrogramDataset(root=args.root, duration=1, data_range=args.data_range)
+    train_root = os.path.join(args.root, 'train')
+    val_root   = os.path.join(args.root, 'val')
+
+    train_dataset = MelSpectrogramDataset(root=train_root, duration=1, data_range=args.data_range)
     trainloader = torch.utils.data.DataLoader(train_dataset,
                                               batch_size=args.batch_size, shuffle=True)
 
-    val_dataset = MelSpectrogramDataset(root=args.root, duration=1, data_range=args.data_range)
+    val_dataset = MelSpectrogramDataset(root=val_root, duration=1, data_range=args.data_range)
     valloader = torch.utils.data.DataLoader(val_dataset,
                                              batch_size=args.batch_size, shuffle=False)
 
