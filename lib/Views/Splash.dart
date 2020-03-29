a06a7993c911d565936293436f86156b5d87dc7b
originx <marioorsolic@gmail.com>
2020-03-29 19:20:21 +0200

    Add ReminderManager for scheduled notifications

--- lib/Views/Splash.dart
+++ lib/Views/Splash.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:iaso/Common/Settings.dart';
+import 'package:iaso/Models/Reminders/ReminderManager.dart';
+
+import '../main.dart';
 
 class SplashScreen extends StatefulWidget {
   SplashScreenState createState() => new SplashScreenState();
@@ -16,11 +19,21 @@ class SplashScreenState extends State<SplashScreen> {
 
   @override
   Widget build(BuildContext context) {
+    reminderManager.requestIOSPermissions();
+    reminderManager.configureDidReceiveLocalNotificationSubject(context);
+    reminderManager.configureSelectNotificationSubject(context);
     return Scaffold(
       body: Container(
         color: Colors.grey[100],
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    didReceiveLocalNotificationSubject.close();
+    selectNotificationSubject.close();
+    super.dispose();
   }
 
   afterBuild(context) {
